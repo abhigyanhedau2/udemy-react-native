a25767d69708cd9eb6f124a368ebb4a810b583e7
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2024-01-07 17:40:01 +0530

    Add Background Image

--- section3-mini-game-app/App.js
+++ section3-mini-game-app/App.js
@@ -1,19 +1,29 @@
-import { StyleSheet } from 'react-native';
-import StartGame from './screens/StartGame';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+
+import StartGame from './screens/StartGame';
 
 export default function App() {
     return <LinearGradient
         colors={["#72063c", "#ddb52f"]}
         style={styles.appContainer}
     >
-        <StartGame />
+        <ImageBackground
+            style={styles.appContainer}
+            source={require("./assets/images/background.png")}
+            resizeMode='cover'
+            imageStyle={styles.imageBackground}
+        >
+            <StartGame />
+        </ImageBackground>
     </LinearGradient>
 }
 
 const styles = StyleSheet.create({
     appContainer: {
-        flex: 1,
-        backgroundColor: "#ddb52f"
+        flex: 1
+    },
+    imageBackground: {
+        opacity: 0.25
     }
 });
