3570070109cae9339521d9c86102b89a7fca24da
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2024-01-12 23:25:25 +0530

    first commit

--- section6-meals-app-context-api/App.js
+++ section6-meals-app-context-api/App.js
@@ -1,6 +1,5 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
