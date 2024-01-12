c1fc7ab60c494f0c41cc14440e7ef0e4d3393a11
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2024-01-12 18:53:21 +0530

    Styling Screen Headers and Background

--- section6-meals-app/App.js
+++ section6-meals-app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -10,12 +10,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     return <>
-        <StatusBar style='dark' />
+        <StatusBar style='light' />
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{
+                headerStyle: { backgroundColor: "#351401" },
+                headerTintColor: "#fff",
+                contentStyle: { backgroundColor: "#3f2f25" }
+            }}>
                 {/* name = name to refer to that component, component = component pointer */}
-                <Stack.Screen name="MealsCategories" component={Categories} />
-                <Stack.Screen name="MealsOverview" component={MealsOverview} />
+                <Stack.Screen name="MealsCategories" component={Categories} options={{ title: "All Categories" }} />
+                <Stack.Screen name="MealsOverview" component={MealsOverview} options={{}} />
             </Stack.Navigator>
         </NavigationContainer>
     </>;
