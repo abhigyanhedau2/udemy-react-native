aac89782ed2f6e6c00891e658ffa00e0e62ae2fd
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2024-01-12 18:21:33 +0530

    Using useRoute Hook

--- section6-meals-app/screens/MealsOverview.js
+++ section6-meals-app/screens/MealsOverview.js
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Text } from "react-native";
+// import { useRoute } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
 
@@ -7,6 +8,9 @@ export default function MealsOverview(props) {
     // route contains a key params that contains any additional params that we might've passed in the
     // navigation.navigate() method
     const categoryId = props.route.params.categoryId;
+
+    // Alternatively, we could also use the useRoute hook to get hold of the route object
+    // const route = useRoute();
 
     return <View style={styles.container}>
         <Text>Meals Overview Screen - {categoryId}</Text>
